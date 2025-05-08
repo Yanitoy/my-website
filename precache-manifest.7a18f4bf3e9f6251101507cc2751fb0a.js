cb8f97a538b92be183308ba031121fca4a8ba0f5
Yanitoy <brianreyfuentesvargas@gmail.com>
2025-05-08 20:05:23 +0800

    Updates

---
+++ precache-manifest.7a18f4bf3e9f6251101507cc2751fb0a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ea97c28d02a2c086a00bb095784cb79a",
+    "url": "/my-website/index.html"
+  },
+  {
+    "revision": "f5c31453d6d01a55fac6",
+    "url": "/my-website/static/css/main.fce00a93.chunk.css"
+  },
+  {
+    "revision": "cf75d6a9dfdaa5b8dca8",
+    "url": "/my-website/static/js/2.b247d883.chunk.js"
+  },
+  {
+    "revision": "f5c31453d6d01a55fac6",
+    "url": "/my-website/static/js/main.e6ca60a3.chunk.js"
+  },
+  {
+    "revision": "37063c54184f9fd8244e",
+    "url": "/my-website/static/js/runtime~main.99d9029d.js"
+  }
+]);
